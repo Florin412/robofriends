4c9fd947cc51593b6658ff438f1f55de6589d3b4
Lupau Remus <lupau.remus15@gmail.com>
2023-11-28 21:17:10 +0200

    CardList component created

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+const Card = ({ id, name, email }) => {
+  return (
+    <div className="bg-success text-center col-11 col-sm-8 col-md-5 col-lg-3 col-xl-3 m-2">
+      <img
+        className="img-fluid"
+        src={`https://robohash.org/${id}`}
+        alt="robot_picture"
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/components/CardList.jsx
@@ -0,0 +1,25 @@
+import Card from "./Card";
+import { robots } from "../robots";
+
+const CardList = () => {
+  const arrayOfRobots = robots.map((robot) => {
+    return (
+      <Card
+        key={robot.id}
+        id={robot.id}
+        name={robot.name}
+        email={robot.email}
+      ></Card>
+    );
+  });
+
+  return (
+    <div className="container">
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 flex-wrap justify-content-center align-items-stretch">
+        {arrayOfRobots}
+      </div>
+    </div>
+  );
+};
+
+export default CardList;
