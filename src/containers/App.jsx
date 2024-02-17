b01b075b59c8c973e3ce91a5a4f5ea2e02ad12bf
Lupau Remus <lupau.remus15@gmail.com>
2024-02-17 16:55:57 +0200

    react hooks added

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,25 +1,43 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../components/CardList";
 import SearchBar from "../components/SearchBar";
 import "./App.css";
 import Scroll from "../components/Scroll";
 import ErrorBoundary from "../components/ErrorBoundary";
 
-class App extends Component {
-  constructor() {
-    super();
+function App() {
+  // constructor() {
+  //   super();
 
-    this.state = {
-      searchField: "",
-      robots: []
-    };
-  }
+  //   this.state = {
+  //     searchField: "",
+  //     robots: []
+  //   };
+  // }
 
-  componentDidMount() {
+  const [robots, setRobots] = useState([]);
+  const [searchField, setSearchField] = useState("");
+
+  // componentDidMount() {
+  //   fetch("https://jsonplaceholder.typicode.com/users")
+  //     .then((res) => res.json())
+  //     .then((res) => {
+  //       this.setState({ robots: res });
+  //     })
+  //     .catch((err) => {
+  //       console.log(
+  //         "There are some errors when you fetch the robots from the server: ",
+  //         err
+  //       );
+  //     });
+  // }
+
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
       .then((res) => {
-        this.setState({ robots: res });
+        setRobots(res);
+        console.log(robots, searchField);
       })
       .catch((err) => {
         console.log(
@@ -27,35 +45,31 @@ class App extends Component {
           err
         );
       });
-  }
+  }, []);
 
-  changeSearchField = (event) => {
-    this.setState({ searchField: event.target.value });
+  const changeSearchField = (event) => {
+    setSearchField(event.target.value);
   };
 
-  render() {
-    const { searchField, robots } = this.state;
-
-    if (robots.length === 0) {
-      return <h1>Loading Page</h1>;
-    }
-
-    const sortedRobots = robots.filter((robot) => {
-      return robot.name.toLowerCase().includes(searchField.toLowerCase());
-    });
-
-    return (
-      <div className="container text-center my-5">
-        <h1 className="fancy-font">ROBOFRIENDS</h1>
-        <SearchBar changeSearchField={this.changeSearchField}></SearchBar>
-        <Scroll>
-          <ErrorBoundary>
-            <CardList robots={sortedRobots}></CardList>
-          </ErrorBoundary>
-        </Scroll>
-      </div>
-    );
+  if (robots.length === 0) {
+    return <h1>Loading Page</h1>;
   }
+
+  const sortedRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchField.toLowerCase());
+  });
+
+  return (
+    <div className="container text-center my-5">
+      <h1 className="fancy-font">ROBOFRIENDS</h1>
+      <SearchBar changeSearchField={changeSearchField}></SearchBar>
+      <Scroll>
+        <ErrorBoundary>
+          <CardList robots={sortedRobots}></CardList>
+        </ErrorBoundary>
+      </Scroll>
+    </div>
+  );
 }
 
 export default App;
